9f303c421f0b099249c236c71b0c175775ae4b3e
poliklinikvildan <134360221+poliklinikvildan@users.noreply.github.com>
2023-06-02 23:39:29 +0300

    Updated booksdb.js file for COURSERA project
    In the updated version of the Coursera project, the original object "books" has been transformed into an array of book objects. Each book object now contains additional properties such as "review" and "isbn". The "review" property provides a brief comment or review about the book, while the "isbn" property represents the International Standard Book Number associated with the book.
    
    For example, the first book in the array (index 0) is "Things Fall Apart" by Chinua Achebe. It has an "id" of 1, a review stating "Great book with a powerful message," and an "isbn" of "100395775590". Similarly, other books in the array have been updated with their respective reviews and ISBN numbers, except for a few cases where the information was not provided by the seller.
    
    Overall, the changes improve the data structure by organizing the books as an array and including additional details like reviews and ISBN numbers, which can be useful for tracking and categorizing the books in the project.

--- final_project/router/booksdb.js
+++ final_project/router/booksdb.js
@@ -1,14 +1,74 @@
-let books = {
-      1: {"author": "Chinua Achebe","title": "Things Fall Apart", "reviews": {} },
-      2: {"author": "Hans Christian Andersen","title": "Fairy tales", "reviews": {} },
-      3: {"author": "Dante Alighieri","title": "The Divine Comedy", "reviews": {} },
-      4: {"author": "Unknown","title": "The Epic Of Gilgamesh", "reviews": {} },
-      5: {"author": "Unknown","title": "The Book Of Job", "reviews": {} },
-      6: {"author": "Unknown","title": "One Thousand and One Nights", "reviews": {} },
-      7: {"author": "Unknown","title": "Nj\u00e1l's Saga", "reviews": {} },
-      8: {"author": "Jane Austen","title": "Pride and Prejudice", "reviews": {} },
-      9: {"author": "Honor\u00e9 de Balzac","title": "Le P\u00e8re Goriot", "reviews": {} },
-      10: {"author": "Samuel Beckett","title": "Molloy, Malone Dies, The Unnamable, the trilogy", "reviews": {} }
-}
+let books = [
+  {
+    id: 1,
+    author: "Chinua Achebe",
+    title: "Things Fall Apart",
+    review: "Great book with a powerful message.",
+    isbn: "100395775590",
+  },
+  {
+    id: 2,
+    author: "Hans Christian Andersen",
+    title: "Fairy Tales",
+    review: "Wonderful collection of enchanting stories.",
+    isbn: "ICBN not provided by seller",
+  },
+  {
+    id: 3,
+    author: "Dante Alighieri",
+    title: "The Divine Comedy",
+    review: "",
+    isbn: "9780142437223",
+  },
+  {
+    id: 4,
+    author: "Unknown",
+    title: "The Epic Of Gilgamesh",
+    review: "",
+    isbn: "ICBN not provided by seller",
+  },
+  {
+    id: 5,
+    author: "Unknown",
+    title: "The Book Of Job",
+    review: "unnecessary, from user-xxxxx",
+    isbn: "21500000",
+  },
+  {
+    id: 6,
+    author: "Unknown",
+    title: "One Thousand and One Nights",
+    review: "",
+    isbn: "9780140449389",
+  },
+  {
+    id: 7,
+    author: "Unknown",
+    title: "Njál's Saga",
+    review: "",
+    isbn: "9780140447699",
+  },
+  {
+    id: 8,
+    author: "Jane Austen",
+    title: "Pride and Prejudice",
+    review: "",
+    isbn: "ISBN not provided by seller",
+  },
+  {
+    id: 9,
+    author: "Honoré de Balzac",
+    title: "Le Père Goriot",
+    review: "A classic French novel.",
+    isbn: "",
+  },
+  {
+    id: 10,
+    author: "Samuel Beckett",
+    title: "Molloy, Malone Dies, The Unnamable, the trilogy",
+    review: "",
+    isbn: "0330256645",
+  },
+];
 
-module.exports=books;
+module.exports = books;
